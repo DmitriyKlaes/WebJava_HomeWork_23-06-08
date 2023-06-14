22ccc33228dbc56198e1f64c0245a6c17029430d
Dmitriy_Klaes <89119584059@mail.ru>
2023-06-14 22:28:09 +0300

    Готово

--- src/main/java/ru/gb/SquareArrayHandler.java
+++ src/main/java/ru/gb/SquareArrayHandler.java
@@ -4,7 +4,7 @@ public class SquareArrayHandler {
 
     public int sumSquareArray(String[][] arrayForSum) throws MyArraySizeException, MyArrayDataException {
         int result = 0;
-        if (isSquareArray(arrayForSum, arrayForSum.length)) {
+        if (isSquareArray(arrayForSum)) {
             for (int i = 0; i < arrayForSum.length; i++) {
                 for (int j = 0; j < arrayForSum.length; j++) {
                     try {
@@ -18,6 +18,16 @@ public class SquareArrayHandler {
         return result;
     }
     
+    private boolean isSquareArray(String[][] arrayForCheck) throws MyArraySizeException {
+        for (String[] nestedArrayStrings : arrayForCheck) {
+            if (arrayForCheck.length != 4 ||
+                    arrayForCheck.length != nestedArrayStrings.length) {
+                throw new MyArraySizeException("Массив не соответствует размеру 4x4");
+            }
+        }
+        return true;
+    }
+
     private boolean isSquareArray(String[][] arrayForCheck, int arraySide) throws MyArraySizeException {
         for (String[] nestedArrayStrings : arrayForCheck) {
             if (arrayForCheck.length != arraySide ||
